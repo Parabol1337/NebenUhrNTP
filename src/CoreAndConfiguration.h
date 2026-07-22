3ffe32acf7c9f5549ff38b20e7ad636c893feffb
Parabol1337 <69092149+Parabol1337@users.noreply.github.com>
2026-07-22 02:57:00 +0200

    Add core and configuration module

---
+++ src/CoreAndConfiguration.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Hardware, SD-Karte, Konfiguration, Status, Logging, Ethernet und NTP.
+// Die beiden Fragmente stammen unverändert aus dem ursprünglichen Sketch.
+#include "part01.inc"
+#include "part02.inc"
+#include "part03.inc"
